88f396df85e81f209b8ba752486db0b440599490
Coetzee, M. (Marcel) <MarcelC@nedbank.co.za>
2021-12-26 11:03:17 +0200

    environment.yml

--- theFunction/dictutil.py
+++ theFunction/dictutil.py
@@ -1,4 +1,14 @@
-# Copyright 2013 Philip N. Klein
-def dict2list(dct, keylist): pass
+# Task 0.6.4
+from typing import Any, List
 
-def list2dict(L, keylist): pass
+
+def dict2list(dct: dict[Any], keylist: List[Any]) -> List[Any]:
+    return [dct[k] for k in keylist]
+
+
+def list2dict(L: List[Any], keylist: List[Any]) -> List[Any]:
+    return {k: v for v, k in zip(L, keylist)}
+
+
+def listrange2dict(L: List[Any]) -> dict[int:Any]:
+    return list2dict(L, range(len(L)))
